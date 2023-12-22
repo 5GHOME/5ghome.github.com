6c7419dd13b6c5009410f000c3f7b4426c0dfc02
5GHOME <154393293+5GHOME@users.noreply.github.com>
2023-12-22 15:07:43 +0800

    Add files via upload

---
+++ page/bilibili-lottery/start/origin.js
@@ -0,0 +1,41 @@
+//log
+console.log('\n' + ' %c Powered by %c JIXIEJIDIGUAN ' + '\n', 'background: linear-gradient(to bottom right, #9FD5F7, #F1CAFF);color:#fff', '');
+console.log('だからもう\n' +
+    '奇跡待たなくてもいいの\n' +
+    '飛び込んで　会いに来て\n' +
+    '渚の果て　何度でも\n' +
+    '歌うから\n' +
+    'いつか想いが叶うまで\n' +
+    '波の翼のマーメイド\n' +
+    '連れてって\n' +
+    'きらめく橋の向こうまで\n' +
+    '一緒に\n');
+console.log('%c links  %c https:/// ', 'color: white; background: #e9546b; padding:5px 0;', 'padding:4px;border:1px solid #e9546b;')
+
+//芜湖
+if (!!window.ActiveXObject || "ActiveXObject" in window) { //is IE?
+    alert('朋友，IE浏览器未适配哦~\n 请关闭 IE 模式！');
+}
+
+//if (document.location.host != "p") {
+//location.href = location.href.replace(document.location.host,'p');
+//}
+
+var OriginTitile = document.title,
+titleTime;
+document.addEventListener("visibilitychange",
+function() {
+    if (document.hidden) {
+        document.title = "Σ( ° △ °|||)真的要走了吗？";
+        clearTimeout(titleTime)
+    } else {
+        document.title = "(✿◡‿◡)欢迎回来";
+        titleTime = setTimeout(function() {
+            document.title = OriginTitile
+        },
+        2000)
+    }
+});
+
+//我要拉
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JMIJZqK4MvC0pOsF\",ck:\"JMIJZqK4MvC0pOsF\"});</script>")
